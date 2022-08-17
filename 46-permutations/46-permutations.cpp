b3d605c7a2f75eb14c8bb9e64ca51067a0652b03
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-08-17 19:52:08 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (37.26%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(vector<int> nums, int pos, vector<vector<int>> &ans){
+        if(pos == nums.size()){
+            ans.push_back(nums);
+        }else{
+            for (int j = pos; j < nums.size(); j++) {
+                swap(nums[pos], nums[j]);
+                solve(nums, pos + 1, ans);
+            }
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        
+        solve(nums, 0, ans);
+        
+        return ans;
+    }
+};
