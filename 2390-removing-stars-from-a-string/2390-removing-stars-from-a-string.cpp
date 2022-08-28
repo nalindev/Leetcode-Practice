14d1efb9616db83fa1b3cdc8e633832e7462c0a5
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-08-28 22:45:43 +0530

    Time: 97 ms (33.33%), Space: 25 MB (16.67%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -7,11 +7,10 @@ public:
         for(int i = s.length()-1; i >= 0; i--){
             if(s[i] == '*') ++count;
             else if(count != 0) --count;
-            else ans += s[i];
+            else ans+=s[i];
         }
         
         reverse(ans.begin(), ans.end());
-        
         return ans;
     }
 };
