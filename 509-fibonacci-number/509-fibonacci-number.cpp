7d9c4eedd21fba4a0a5d6fdc89887deee9be89af
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-07 01:01:19 +0530

    Time: 2 ms (66.32%), Space: 6.1 MB (17.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0) return 0;
+        //recursive approach
+//         if(n == 0) return 0;
         
-        if(n == 1 || n == 2) return 1;
+//         if(n == 1 || n == 2) return 1;
         
-        return fib(n-1)+fib(n-2);
+//         return fib(n-1)+fib(n-2);
+        
+        //DP approach
+        vector<int> v(31);
+        v[0] = 0;
+        v[1] = 1;
+        
+        int ans = 0;
+        for(int i = 2; i <= n; i++) v[i] = v[i-1]+v[i-2];
+        
+        return v[n];
     }
 };
