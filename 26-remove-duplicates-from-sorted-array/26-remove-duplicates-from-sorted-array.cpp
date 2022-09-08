0a027fa5bd375b996e15442e1fde591295da2809
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-09 00:48:30 +0530

    Time: 28 ms (23.85%), Space: 18.3 MB (74.79%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -3,15 +3,22 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int f = 0, s = 0, n = nums.size();
         
-        while(s < n){
-            if(nums[f] == nums[s]){
-                ++s;
-            }else{
-                nums[f+1] = nums[s];
+        // while(s < n){
+        //     if(nums[f] == nums[s]){
+        //         ++s;
+        //     }else{
+        //         nums[f+1] = nums[s];
+        //         ++f;
+        //         ++s;
+        //     }
+        // } 
+        
+        for(int i = 0; i < n; i++){
+            if(nums[f] != nums[i]){
+                nums[f+1] = nums[i];
                 ++f;
-                ++s;
             }
-        } 
+        }
         return f+1;
     }
 };
