33243838b604129d73baf07b0564cb2beae58c39
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-09 00:53:46 +0530

    Time: 38 ms (11.24%), Space: 18.4 MB (36.69%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int f = 0, s = 0, n = nums.size();
-        
+        // int f = 0, s = 0, n = nums.size();
         // while(s < n){
         //     if(nums[f] == nums[s]){
         //         ++s;
@@ -12,23 +11,24 @@ public:
         //         ++s;
         //     }
         // } 
+        //return f+1;
         
         //optimized it further but the running time is still same
+        // int f = 0, s = 0, n = nums.size();
         // for(int i = 0; i < n; i++){
         //     if(nums[f] != nums[i]){
         //         nums[f+1] = nums[i];
         //         ++f;
         //     }
         // }
-        
-        //looking for other solution 
-        int count = 0;
-for(int i = 1; i < n; i++){
-    if(nums[i] == nums[i-1]) count++;
-    else nums[i-count] = nums[i];
-}
-return n-count;
-        
         //return f+1;
+        
+        //looking for other solution - This code have better approach then me but complexity is same 
+        int count = 0, n = nums.size();
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]) count++;
+            else nums[i-count] = nums[i];
+        }
+        return n-count;
     }
 };
