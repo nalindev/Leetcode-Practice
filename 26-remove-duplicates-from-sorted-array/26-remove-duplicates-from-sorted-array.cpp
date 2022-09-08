d927edd496ef813f92fc694b05ec455680758308
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-09 00:51:36 +0530

    Time: 30 ms (19.66%), Space: 18.4 MB (74.79%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -13,12 +13,22 @@ public:
         //     }
         // } 
         
-        for(int i = 0; i < n; i++){
-            if(nums[f] != nums[i]){
-                nums[f+1] = nums[i];
-                ++f;
-            }
-        }
-        return f+1;
+        //optimized it further but the running time is still same
+        // for(int i = 0; i < n; i++){
+        //     if(nums[f] != nums[i]){
+        //         nums[f+1] = nums[i];
+        //         ++f;
+        //     }
+        // }
+        
+        //looking for other solution 
+        int count = 0;
+for(int i = 1; i < n; i++){
+    if(nums[i] == nums[i-1]) count++;
+    else nums[i-count] = nums[i];
+}
+return n-count;
+        
+        //return f+1;
     }
 };
