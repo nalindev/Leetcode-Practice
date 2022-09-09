5e554067e1a8a55d147098c8fc290581daf2f18a
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-10 00:12:34 +0530

    Time: 20 ms (89.71%), Space: 18 MB (20.21%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        set<int> s;
-        for(auto x : nums) s.insert(x);
+        // O(n) Time complexity & O(n) space
+        //set<int> s;
+        //for(auto x : nums) s.insert(x);
+        //for(int i = 0; i <= nums.size(); i++) if(!s.count(i)) return i;
+        //return -1;
         
-        for(int i = 0; i <= nums.size(); i++) if(!s.count(i)) return i;
+        // Bit Manipulation O(n) Time Complexity & O(1) space 
+        int temp = 0;
         
-        return -1;
+        for(int i = 1; i < nums.size()+1; i++){
+            temp ^= i;
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            temp ^= nums[i];
+        }
+        
+        return temp;
     }
 };
