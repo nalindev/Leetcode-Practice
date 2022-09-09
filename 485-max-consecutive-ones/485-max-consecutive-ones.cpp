131cd9d96efa2827db848d6be2ebbdacf557066f
Nalin Nishant <86837840+nalindev@users.noreply.github.com>
2022-09-10 00:34:20 +0530

    Time: 75 ms (27.90%), Space: 36.2 MB (70.69%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -4,13 +4,9 @@ public:
         int ans = 0, count = 0;
         
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 1) ++count;
-            else{
-                ans = max(ans, count);
-                count = 0;
-            }
+            if(nums[i] == 1) ans = max(++count, ans);
+            else count = 0;
         }
-        if(count) ans = max(ans, count);
         return ans;
     }
 };
